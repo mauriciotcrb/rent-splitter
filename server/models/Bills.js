6a01297b03ca0c24209da043c93d66d5b5236f71
Mauricio Torres Cande <mautc16@me.com>
2025-05-24 13:47:31 -0700

    Add Bill model for rent and bill tracking

---
+++ server/models/Bills.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const billSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  household: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Household',
+    required: true,
+  },
+  paidBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  splitBetween: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Bill', billSchema);
